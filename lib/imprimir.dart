baf02235f425dec5cf75c16fd8e509f9ab611867
Aline Rose Alencar Santos <alineralncs@gmail.com>
2021-07-22 15:43:15 -0300

    Create imprimir.dart

---
+++ lib/imprimir.dart
@@ -0,0 +1,27 @@
+menu() {
+  //print('iList - Lista de Compras');
+  print('====== ILIST - MENU ======');
+  print('1 - Entrar');
+  print('2 - sair');
+  print('==================');
+}
+
+miniMenu() {
+  print('==== Deseja entrar no <iList>? ====');
+  print('1 - Sim');
+  print('2 - Não');
+}
+
+ilistMenu() {
+  print('==============');
+  print('1 - Inserir');
+  print('2 - Remover');
+  print('3 - Ver Tamanho');
+  print('4 - Ver lista');
+  print('5 - Sair');
+  print('==============');
+}
+
+funPrint() {
+  print('\x1B[2J\x1B[0;0H');
+}
